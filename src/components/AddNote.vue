f6afae1f278ebb05848b5bad2677a4d39330168d
Alvar Sirlin <alvar@roswellstudios.com>
2023-07-03 18:53:53 -0400

    initial AddNote component form

---
+++ src/components/AddNote.vue
@@ -0,0 +1,62 @@
+<script>
+import { ref } from 'vue'
+// import Component from "Component"
+
+
+export default {
+	name: "AddNote",
+	components: {
+		// Component,
+	},
+	props: { 
+		timestamp: Number,
+	},
+	data() {
+		return {
+			timestampValue: "",
+			tags: "",
+			entry: '',
+		}
+	},
+	mounted() {
+		this.timestampValue = this.timestamp.toString();
+	}
+}
+
+
+
+
+
+
+</script>
+
+<template>
+  <h2>Add Entry</h2>
+	<form id='add-note' @submit.prevent="submitForm">
+		<div>
+			<label for='time'>Time</label>
+			<input id='time' type="text" placeholder="timestampValue" v-model="timestampValue" required>
+			<em>V-model sanity check: {{timestampValue}}</em>
+		</div>
+		<div>
+			<label for="tags">Tags</label>
+			<input type="text" v-model='tags' required>
+			<em>V-model sanity check: {{tags}}</em>
+		</div>
+		<div>
+			<label for="entry">Entry</label>
+			<textarea id="entry" name="" cols="30" rows="10" v-model='entry' required></textarea>
+			<em>V-model sanity check: {{entry}}</em>
+		</div>
+	</form>
+</template>
+
+<style scoped>
+#add-note * {
+	display:block;
+}
+
+#add-note label {
+	margin-top: 20px;
+}
+</style>
