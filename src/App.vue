48c1a83dc493429ff294adf3bedba73de8f7291e
Alvar Sirlin <alvar@roswellstudios.com>
2023-07-09 17:21:36 -0400

    addNote submit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Flogger!</h1>
   </div>
-  <AddNote :timestamp="timestamp" />
+  <AddNote @newNote='addNewNote' :timestamp="timestamp" />
   <NoteList :notes="testNotes" />
 </template>
 
@@ -18,23 +18,34 @@ export default {
 
   data() {
     return {
-      timestamp: Date.now(),
+      timestamp: new Date(),
       testNotes: [
       {
           timestamp: new Date(),
           tags: ["tag 1", "tag 2"],
           message: "This is the note body message.",
         },
-        {
-          timestamp: new Date(),
-          tags: ["tag 1", "tag 3"],
-          message: "This is the note TWO body message.",
-        },
+       // {
+         // timestamp: new Date(),
+          //tags: ["tag 1", "tag 3"],
+          //message: "This is the note TWO body message.",
+        //},
       ],
     };
   },
 
-  methods: {},
+  methods: {
+		addNewNote(noteData) {
+			// console.log(noteData.timestamp);
+			this.testNotes.push ({ 
+				timestamp: Date(noteData.timestamp),
+				tags: noteData.tags.split(" "),
+				message: noteData.entry
+			});
+
+
+		}
+  },
 };
 </script>
 
--- src/components/AddNote.vue
+++ src/components/AddNote.vue
@@ -2,63 +2,54 @@
 import { ref } from 'vue'
 // import Component from "Component"
 
-
 export default {
 	name: "AddNote",
 	components: {
 		// Component,
 	},
 	props: { 
-		timestamp: Number,
+		timestamp: Date,
 	},
+	// emits: ['newNote'],
 	data() {
 		return {
-			timestampValue: "",
-			tags: [],
 			newTag: '',
-			entry: '',
+			form: {
+				timestampValue: this.timestamp,
+				tags: '',
+				entry: '',
+			}
 		}
 	},
- methods: {
+	methods: {
 		 addTag() {
 			  this.tags.push(this.newTag);
 			  this.newTag = '';
 		 },
+		 submitAdd(event) {
+			 this.$emit('newNote', this.form);
+		 }
 	},
-	mounted() {
-		this.timestampValue = this.timestamp.toString();
-	}
 }
-
-
-
-
-
-
 </script>
 
 <template>
   <h2>Add Entry</h2>
-	<form id='add-note' @submit.prevent="submitForm">
+	<form id='add-note' @submit.prevent="submitAdd">
 		<div>
 			<label for='time'>Time</label>
-			<input id='time' type="text" placeholder="timestampValue" v-model="timestampValue" required>
-			<em>V-model sanity check: {{timestampValue}}</em>
+			<input id='time' type="text" placeholder="timestampValue" v-model="form.timestampValue" required >
 		</div>
 		<div>
 			<label for="tags">Tags</label>
-			<input type="text" v-model="newTag" @keyup.enter="addTag" required>
-			<em>V-model sanity check: {{newTag}}</em>
-			<em>Array:</em>
-			    <ul>
-       <li v-for="tag in tags" :key="tag">{{ tag }}</li>
-    </ul>
+			<input type="text" v-model="form.tags">
 		</div>
 		<div>
 			<label for="entry">Entry</label>
-			<textarea id="entry" name="" cols="30" rows="10" v-model='entry' required></textarea>
-			<em>V-model sanity check: {{entry}}</em>
+			<textarea id="entry" name="" cols="30" rows="10" v-model='form.entry'></textarea>
 		</div>
+		<div><input type="submit"></div>
+		
 	</form>
 </template>
 
