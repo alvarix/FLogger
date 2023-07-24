5795ce1a553f396292725a227849682490d89db4
Alvar Sirlin <alvar@roswellstudios.com>
2023-07-23 20:09:45 -0400

    notData model

---
+++ src/modules/NoteData.js
@@ -0,0 +1,13 @@
+<script>
+export default class NoteData {
+	date;
+	tags;
+	entry;
+
+  constructor(date, tags, entry) {
+    this.date = date;
+    this.tags = tags;
+    this.entry = entry;
+  }
+}
+</script>
---
+++ src/modules/NoteData.vue
@@ -0,0 +1,13 @@
+class NoteModel {
+
+	date;
+	tags;
+	entry;
+
+
+  constructor(date, tags, entry) {
+    this.date = date;
+    this.tags = tags;
+    this.entry = entry;
+  }
+}
