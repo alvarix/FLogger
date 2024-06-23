476c8bedbc8d60513e832f28e91a8e5087f24334
CCC <chad@roswellstudios.com>
2024-06-23 11:59:20 -0400

    Auth and list files working

--- src/components/UserAuth.vue
+++ src/components/UserAuth.vue
@@ -7,7 +7,9 @@ const props = defineProps({});
 
 const hostname = "localhost";
 const port = 5173;
-var CLIENT_ID = "q5qja4ma5qcl0qc"; //flogger-chad: q5qja4ma5qcl0qc //ORIGINAL EXAMPLE: 42zjexze6mfpf7x
+var CLIENT_ID = "irjhf3obwytvv53"; //flogger-ccc4
+//"lsu851xgok0qryy"; //Flogger Starscream
+//"k2i486lvdpfjyhj"; //"q5qja4ma5qcl0qc"; //flogger-chad: q5qja4ma5qcl0qc //ORIGINAL EXAMPLE: 42zjexze6mfpf7x
 
 const config = {
   fetch,
@@ -15,6 +17,7 @@ const config = {
 };
 
 import { Dropbox, DropboxAuth } from "dropbox";
+// See https://dropbox.github.io/dropbox-sdk-js/Dropbox.html
 
 const dbxAuth = new DropboxAuth(config);
 
@@ -33,8 +36,9 @@ const doAuth = () => {
       true
     )
     .then((authUrl) => {
-      window.sessionStorage.clear();
+      clearDbxSession();
       window.sessionStorage.setItem("codeVerifier", dbxAuth.codeVerifier);
+      console.log("dbxAuth.codeVerifier", dbxAuth.codeVerifier);
       window.location.href = authUrl;
     })
     .catch((error) => console.error(error));
@@ -42,60 +46,148 @@ const doAuth = () => {
 
 // Parses the url and gets the access token if it is in the urls hash
 const getDbxAuthCodeFromUrl = () => {
-  //   return utils.parseQueryString(window.location.search).code;
-  //   const code = qs.parse(window.location.search)?.code;
-  //   console.log(`window.location.search: ${window.location.search}`);
-  //   console.log(`qs.parse(window.location.search):`, qs.parse(window.location.search));
-  //   console.log(`getDbxAuthCodeFromUrl: ${code}`);
-  //   qs.parse(window.location.search,{})
-  const query = window.location.search.split("&");
-  const code = query[0].split("=")[1];
-  //   console.log(`getDbxAuthCodeFromUrl: ${code}`);
+  const params = new URL(window.location).searchParams;
+  const code = params.get("code");
+  // console.log(`getDbxAuthCodeFromUrl: ${code}`);
   return code;
+};
+const removeAuthCodeFromUrl = (urlString) => {
+  let url = new URL(urlString);
+  // console.log("url before", url.toString());
+  url.searchParams.delete("code");
+  // console.log("url after", url.toString());
+  return url.toString();
 };
 
 const dbxAuthCode = ref(getDbxAuthCodeFromUrl());
 const hasRedirectedFromAuth = ref(!!dbxAuthCode.value);
 
 const fileItems = ref([]);
+const fileItemThumbnails = ref({});
 
 if (hasRedirectedFromAuth.value) {
   console.log(`dbxAuthReturnUri`, dbxAuthReturnUri);
   console.log(`dbxAuthCode`, dbxAuthCode.value);
 
-  const codeVerifier = window.sessionStorage.getItem("codeVerifier")
-  console.log(`codeVerifier:`, codeVerifier)
+  const codeVerifier = window.sessionStorage.getItem("codeVerifier");
+  console.log(`codeVerifier:`, codeVerifier);
   dbxAuth.setCodeVerifier(codeVerifier);
-  console.log('step 1')
+  const reloadUrl = removeAuthCodeFromUrl(window.location.href);
+  console.log("reloadUrl", reloadUrl);
+  console.log("step 1");
   dbxAuth
     // 1. Get token
-    .getAccessTokenFromCode(dbxAuthReturnUri, dbxAuthCode)
-    // 2. Use token to get files
+    .getAccessTokenFromCode(dbxAuthReturnUri, dbxAuthCode.value)
+    // 2. Save token and reload
     .then((response) => {
-      console.log('step 2')
-      dbxAuth.setAccessToken(response.result.access_token);
-      console.log('step 3')
-      var dbx = new Dropbox({
-        auth: dbxAuth,
-      });
-      return dbx.filesListFolder({
-        path: "",
-      });
+      console.log("step 2");
+      window.sessionStorage.setItem(
+        "accessToken",
+        response.result.access_token
+      );
+      window.location.href = removeAuthCodeFromUrl(window.location.href);
     })
-    // 3. Set fileItems to display
-    .then((response) => {
-      console.log('step 4')
-      fileItems.value = response.result.entries.map((item) => {
-        return {
-          name: item.name,
-          itemUri: item.preview_url,
-        };
-      });
-    })
-    // .catch((error) => {
-    //   console.error(error.error || error);
-    // });
+    .catch((e) => {
+      console.log("Error getting access token from URL:", e.error || e);
+      window.location.href = removeAuthCodeFromUrl(window.location.href);
+    });
+  // .catch((error) => {
+  //   console.error(error.error || error);
+  // });
 }
+
+// 3. Get token
+let accessToken = window.sessionStorage.getItem("accessToken");
+if (accessToken && accessToken != "") {
+  console.log("step 3a from sessionStorage");
+  dbxAuth.setAccessToken(accessToken);
+} else {
+  console.log("step 3b check from dbxAuth");
+  window.sessionStorage.removeItem("accessToken");
+  accessToken = dbxAuth.getAccessToken();
+}
+
+console.log("accessToken:", accessToken);
+
+if (accessToken) {
+  // 4. Check/refresh token
+  dbxAuth.checkAndRefreshAccessToken();
+  // 5. Use token to get files
+  console.log("step 5");
+  var dbx = new Dropbox({
+    auth: dbxAuth,
+  });
+  dbx
+    .filesListFolder({
+      path: "",
+    })
+    // 6. Set fileItems to display
+    .then((response) => {
+      console.log("step 6");
+      fileItems.value = response.result.entries.map((item) => {
+        console.log("item", item);
+        return item;
+      });
+      dbxAuth.checkAndRefreshAccessToken();
+      dbx
+        .filesGetThumbnailBatch({
+          entries: response.result.entries.map((item) => {
+            return {
+              /**
+               * The path to the image file you want to thumbnail.
+               */
+              path: item.path_lower,
+              /**
+               * Defaults to TagRef(Union('ThumbnailFormat', [UnionField('jpeg', Void,
+               * False, None), UnionField('png', Void, False, None)]), 'jpeg').
+               */
+              // format?: ThumbnailFormat;
+              /**
+               * Defaults to TagRef(Union('ThumbnailSize', [UnionField('w32h32', Void,
+               * False, None), UnionField('w64h64', Void, False, None),
+               * UnionField('w128h128', Void, False, None), UnionField('w256h256', Void,
+               * False, None), UnionField('w480h320', Void, False, None),
+               * UnionField('w640h480', Void, False, None), UnionField('w960h640', Void,
+               * False, None), UnionField('w1024h768', Void, False, None),
+               * UnionField('w2048h1536', Void, False, None)]), 'w64h64').
+               */
+              // size?: ThumbnailSize;
+              /**
+               * Defaults to TagRef(Union('ThumbnailMode', [UnionField('strict', Void,
+               * False, None), UnionField('bestfit', Void, False, None),
+               * UnionField('fitone_bestfit', Void, False, None)]), 'strict').
+               */
+              // mode?: ThumbnailMode;
+            };
+          }),
+        })
+        .then((response) => {
+          console.log("filesGetThumbnailBatch", response);
+          response.result.entries.forEach((entry) => {
+            if (entry[".tag"] == "success") {
+              fileItemThumbnails.value[entry.metadata.id] = entry.thumbnail;
+            }
+          });
+          return true;
+        })
+        .then(() => {
+          dbxAuth.checkAndRefreshAccessToken();
+          dbx.fileRequestsCreate;
+        });
+    });
+}
+
+const clearDbxSession = () => {
+  console.log('clearDbxSession');
+  window.sessionStorage.removeItem("accessToken");
+  window.sessionStorage.removeItem("codeVerifier");
+  dbxAuthCode.value = undefined;
+  accessToken = undefined;
+  fileItems.value = [];
+  fileItemThumbnails.value = {};
+};
+
+
 </script>
 
 <template>
@@ -104,7 +196,7 @@ if (hasRedirectedFromAuth.value) {
     <p>This example shows how to use PKCE in the browser:</p>
     <div
       id="pre-auth-section"
-      :style="{ display: hasRedirectedFromAuth ? 'none' : 'block' }"
+      :style="{ display: accessToken ? 'none' : 'block' }"
     >
       <button @click="doAuth">Start PKCE Auth Flow</button>
 
@@ -116,15 +208,31 @@ if (hasRedirectedFromAuth.value) {
 
     <div
       id="authed-section"
-      :style="{ display: hasRedirectedFromAuth ? 'block' : 'none' }"
+      :style="{ display: accessToken ? 'block' : 'none' }"
     >
       <p>
         You have successfully authenticated. Below are the contents of your root
         directory. They were fetched using the SDK and access token.
       </p>
+      <div>{{ JSON.stringify(fileItemThumbnails.value) }}</div>
       <ul id="files">
-        <li v-for="item in fileItems">{{ item.name }} -- {{ item.itemUri }}</li>
+        <li v-for="item in fileItems">
+          <b>{{ item.path_display }}</b>
+          <ul class="item">
+            <li>type (".tag"): {{ item[".tag"] }}</li>
+            <li>name: {{ item.name }}</li>
+            <li>path_display: {{ item.path_display }}</li>
+            <li>path_lower: {{ item.path_lower }}</li>
+            <li>id: {{ item.id }}</li>
+            <li v-if="fileItemThumbnails[item.id]">
+              <img
+                :src="'data:image/jpeg;base64,' + fileItemThumbnails[item.id]"
+              />
+            </li>
+          </ul>
+        </li>
       </ul>
+      <button @click="clearDbxSession">clear</button>
     </div>
   </section>
 </template>
@@ -145,5 +253,12 @@ input.error {
 
 #add-note label {
   margin-top: 20px;
+}
+
+ul {
+  border-radius: 14px;
+  list-style: disc;
+  padding: 0.5em;
+  margin: 0.5em;
 }
 </style>
