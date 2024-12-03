9101dd42c8fc984360096f13c35f90f0d1bf0c75
Alvar Sirlin <alvar@alvarsirlin.com>
2024-12-03 14:35:11 -0500

    convert styles to stylus

--- src/App.vue
+++ src/App.vue
@@ -45,101 +45,75 @@ h1 pre
   font-size 8px
   font-family monospace
 
-h2 {
-  font-size: 2em;
-}
+  h2
+    font-size 2em
+    font-size 2.8em
 
-* {
-  font-family: ui-monospace, "Cascadia Code", "Source Code Pro", Menlo, Consolas,
-    "DejaVu Sans Mono", monospace;
-}
+*
+    font-family ui-monospace, "Cascadia Code", "Source Code Pro", Menlo, Consolas,
+    "DejaVu Sans Mono", monospace
 
-:root {
-  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
-  line-height: 1.5;
-  font-weight: 400;
+:root
+    font-family Inter, system-ui, Avenir, Helvetica, Arial, sans-serif
+    line-height 1.5
+    font-weight 400
+    color-scheme light dark
+    color rgba(255, 255, 255, 0.87)
+    background-color #242424
+    font-synthesis none
+    text-rendering optimizeLegibility
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    -webkit-text-size-adjust 100%
 
-  color-scheme: light dark;
-  color: rgba(255, 255, 255, 0.87);
-  background-color: #242424;
+a
+    font-weight 500
+    color #646cff
+    text-decoration inherit
+    font-weight 500
+    color #646cff
+    text-decoration inherit
+    &:hover
+        color #535bf2
+        color #535bf2
 
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
-}
+body
+    margin 0
+    display flex
+    min-width 320px
+    min-height 100vh
+    background-color Canvas
+    color CanvasText
+    color-scheme light dark
 
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
-a:hover {
-  color: #535bf2;
-}
+h1, h2, h3, h4
+    line-height 1.1
+    padding 1em 0
 
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
-a:hover {
-  color: #535bf2;
-}
+h1
+    font-size 3.5em
 
-body {
-  margin: 0;
-  display: flex;
-  min-width: 320px;
-  min-height: 100vh;
-}
+h3
+    font-size 2.1em
 
-h1, h2, h3, h4 {
-  line-height: 1.1;
-  padding: 1em 0;
-}
-h1 {
-  font-size: 3.5em;
-}
-h2 {
-  font-size: 2.8em;
-}
-h3 {
-  font-size: 2.1em;
-}
-h4 {
-  font-size: 1.4em;
-}
+h4
+    font-size 1.4em
 
+#app
+    max-width 1280px
+    margin 0 auto
+    padding 2rem
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-}
+button
+    cursor pointer
+    font-size 1.2rem
+    padding 10px 15px
 
-button { 
-  cursor: pointer;
-  font-size: 1.2rem;
-  padding: 10px 15px;
-}
+@media (prefers-color-scheme: light)
+    button
 
-@media (prefers-color-scheme: light) {
+/* Tailwind */
+.hidden
+    display none
 
-  button {
-  }
-}
-
-body {
-  background-color: Canvas;
-  color: CanvasText;
-  color-scheme: light dark;
-}
-
-
-/* TW */
-.hidden {
-  display: none;
-}
 </style>
