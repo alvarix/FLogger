9286304cde70ac3f9e5de1d3e99e0c65e72aa053
Alvar Sirlin <alvar@alvarsirlin.com>
2024-12-24 14:27:20 -0500

    add intro component for hp content

--- src/components/DropBoxFlogs.vue
+++ src/components/DropBoxFlogs.vue
@@ -75,20 +75,8 @@ function handleAddFlog(flogData) {
       id="pre-auth-section"
       :style="{ display: hasConnection ? 'none' : 'block' }"
     >
-      <p>
-        Welcome to Flogger!<br />
-        A project by
-        <a href="https://alvarsirlin.com" target="_blank">Alvar</a> and
-        <a href="https://chad.crume.org/" target="_blank">Chad</a>.<br />
-        See the
-        <a href="https://github.com/alvarix/FLogger/" target="_blank">Github</a>
-        page for more info.
-      </p>
-      <p>Connect to your DropBox account to begin.</p>
-
-      <button class="dbx__btn" @click="launchConnectFlow">
-        connect to DropBox
-      </button>
+      <Intro />
+  
     </div>
 
     <div
