d37dfdf6afffd28fd5831a8e6680105095c2b26f
CCC <chad@roswellstudios.com>
2025-01-07 13:29:52 -0500

    Updated TestParse component, for reference later with testing framework

--- src/components/TestParse.vue
+++ src/components/TestParse.vue
@@ -1,16 +1,12 @@
 <script setup>
+import { deserializeEntries } from "@/modules/Flog";
 // import { IEntry } from '@/modules/EntryData'
 
-function isValidDate(dateString) {
-  const date = new Date(dateString);
-  return !isNaN(date.getTime());
-}
-
-const testData = `Workout 2.1
+const rawEntryContent0 = `Workout 2.1
 
 
 12/21/24
-- Blink 
+- Blink
 - 915-
 - :)7.5
 - [ ] Hip thrust .6-3
@@ -21,23 +17,23 @@ const testData = `Workout 2.1
 - [ ] < S Leg X
 (
 - [ ] DB Twist
-- [ ] < Inverted Row 
+- [ ] < Inverted Row
 - [ ] Plank Reach .10
 )
-+  chores 
++  chores
 - :)7.5
 
 12/14/24
-- Mtk 
+- Mtk
 - 915-1115
-- :)6 
+- :)6
 - - hemmies
 - [ ] + sprints 20s-4
 3-5 Sets of each between 6-10 reps, 1:30-2 minutes of rest between sets
 - [x] > Curtsy Lunge .6-4 15! ^30!
 ->
 - [x] Lunge Twist .5 ^:15!
-- L glute $4 R ham $2.5 from 3 days ago 
+- L glute $4 R ham $2.5 from 3 days ago
 - [x] > DB Stairs .3-4 ^25! 30!
 2-4 sets of each between 8-15 reps, 1-1:30 minutes of rest
 (
@@ -53,7 +49,7 @@ Accessory: 2-3 sets of each between 12-20 reps, 1 minute rest
 - [x] > OH Tricep .20 20! 25!
 - [x] Plank Reach .10 .12
 }
-+  chores 
++  chores
 - :)7.5
 
 
@@ -62,9 +58,9 @@ Accessory: 2-3 sets of each between 12-20 reps, 1 minute rest
 
 
 12/8/24
-- Mtk 
+- Mtk
 - 1110-1240
-- 6:) 
+- 6:)
 - - Still sore and hemmies $6
 + Box
 3-5 Sets of each between 6-10 reps, 1:30-2 minutes of rest between sets
@@ -72,7 +68,7 @@ Accessory: 2-3 sets of each between 12-20 reps, 1 minute rest
 - [ ] < Back Squat .8-3
 - [x] > Curtsy Lunge .6-4 15!
 - [ ] | Lunge Twist
-- L glute $4 R ham $2.5 from 3 days ago 
+- L glute $4 R ham $2.5 from 3 days ago
 - [x] > DB Stairs .3-4 ^25!
 2-4 sets of each between 8-15 reps, 1-1:30 minutes of rest
 (
@@ -85,70 +81,141 @@ Accessory: 2-3 sets of each between 12-20 reps, 1 minute rest
 { .20–2
 Accessory: 2-3 sets of each between 12-20 reps, 1 minute rest
 - [ ] < S Leg Press
-- [ ] < Inverted Row 
-- [x] > ATG Lunge .15! 
-- raised 
+- [ ] < Inverted Row
+- [x] > ATG Lunge .15!
+- raised
 - [x] > OH Tricep .20 20!
 - [x] Plank Reach .10
 }
-+ chores 
++ chores
 - 7.5:)`;
 
-// The following few steps convert array of
-//    [date,entry,date,entry,...]
-// into an array of objects with date and entry properties:
-//    [{date, entry},{date, entry},...]
-// Doing it in steps for better error handling
-let splitItems, pretext, splitMappedItems, splitMappedFilteredItems;
+const rawEntryContentX = `
+10/30/2024
+One linebreaks in pretext
+
+10/30/2024
+Save 4
+
+10/30/2024
+Save 3
+
+10/30/2024
+Save 2
+
+
+
+5/2/2024
+Something else
+
+
+
+
+10/9/2024
+db from form
+
+10/10/2024
+Something else
+
+
+`;
+
+const rawEntryContentEmpty = ``;
+
+const rawEntryContent1 = `10/30/2024
+No pretext at all
+
+10/30/2024
+middle entry
+
+10/28/2024
+earliest entry
+`;
+
+const rawEntryContent2 = `
+10/30/2024
+One linebreak only pretext
+
+10/30/2024
+middle entry
+
+10/28/2024
+earliest entry
+`;
+
+const rawEntryContent3 = `
+
+10/30/2024
+Two linebreaks only pretext
+
+10/30/2024
+middle entry
+
+10/28/2024
+earliest entry
+`;
+
+const rawEntryContent4 = `
+Pretext with starting linebreak
+more
+
+more
+
+10/30/2024
+latest entry
+
+10/30/2024
+middle entry
+
+10/28/2024
+earliest entry
+`;
+
+function isValidDate(dateString) {
+  // const date = new Date(dateString);
+  // return !isNaN(date.getTime());
+  return /([0-1]?[0-9]\/[0-3]?[0-9]\/[0-9]{4}|[0-1]?[0-9]\/[0-3]?[0-9]\/[0-9]{2})/.test(
+    dateString
+  );
+}
+let filteredEntries, pretext;
+let splitItems, filteredItems, itemsMappedToEntries;
 try {
-  splitItems = testData.split(
+  // The following few steps convert array of
+  //    [date,entry,date,entry,...]
+  // into an array of objects with date and entry properties:
+  //    [{date, entry},{date, entry},...]
+
+  splitItems = rawEntryContent0.split(
     /(?<!.)([0-1]?[0-9]\/[0-3]?[0-9]\/[0-9]{4}|[0-1]?[0-9]\/[0-3]?[0-9]\/[0-9]{2})\n|\n\n([0-1]?[0-9]\/[0-3]?[0-9]\/[0-9]{4}|[0-1]?[0-9]\/[0-3]?[0-9]\/[0-9]{2})\n/
   );
-
-  let firstEntryIndex;
-  splitMappedItems = splitItems.map((item, index, arr) => {
+  filteredItems = splitItems.filter((item) => !!item);
+  itemsMappedToEntries = filteredItems.map((item, index, arr) => {
     if (isValidDate(item) && arr[index + 1]) {
-      firstEntryIndex = firstEntryIndex || index;
       return { date: new Date(arr[index]), entry: arr[index + 1] };
     }
   });
-  splitMappedFilteredItems = splitMappedItems.filter((item) => !!item);
+  filteredEntries = itemsMappedToEntries.filter((item) => !!item);
 
-  pretext = splitItems.reduce((prev, current, index) => {
-    if (index < firstEntryIndex) {
-      console.log("index", index, current);
-      console.log((prev || "") + (current || ""));
-      // UNRESOLVED: Not sure if this should insert the \n or not
-      return (prev || "") + (current || "") + "\n";
-    } else {
-      return prev;
+  // "pretext" found before the first date is not returned by this deserializeEntries function (... yet?)
+  // But this is how it can be parsed out here, after already splitting by date:
+  let firstEntryFound = false;
+  pretext = splitItems.reduce((prev, item, index, arr) => {
+    if (!firstEntryFound && isValidDate(item)) {
+      firstEntryFound = true;
     }
-  }, "");
-  console.log("pretext", pretext);
-
-  // let splitFilteredItems,
-  //   splitFilteredMappedItems,
-  //   splitFilteredMappedFilteredItems;
-
-  // // Filter out empty items and non-date initial items (pretext)
-  // splitFilteredItems = splitItems.filter(
-  //   (item, index) => item && item != "\n" && !(index == 0 && !isValidDate(item))
-  // );
-  // // Convert array of [date,entry,date,entry,...] into [{date, entry},{date, entry},...]
-  // splitFilteredMappedItems = splitFilteredItems.map((item, index, arr) =>
-  //   index % 2 == 1
-  //     ? { date: new Date(arr[index]), entry: arr[index + 1] }
-  //     : undefined
-  // );
-  // // Filter out undefined remnants
-  // splitFilteredMappedFilteredItems = splitFilteredMappedItems.filter(
-  //   (item) => !!item
-  // );
-
-  // pretext = splitItems[0] || undefined;
+    if (firstEntryFound) {
+      return prev;
+    } else {
+      return (prev || "") + (item || "") + "\n";
+    }
+  }, undefined);
 } catch (e) {
   console.log("Error parsing flogger file content", e);
+  console.log("rawEntryContent0", rawEntryContent0);
+  // return [];
 }
+
 </script>
 
 <template>
@@ -157,13 +224,17 @@ try {
     <ol>
       <li v-for="item in splitItems">{{ item }}</li>
     </ol>
-    <h2>splitMappedItems</h2>
+    <h2>filteredItems</h2>
     <ol>
-      <li v-for="item in splitMappedItems">{{ item }}</li>
+      <li v-for="item in filteredItems">{{ item }}</li>
     </ol>
-    <h2>splitMappedFilteredItems</h2>
+    <h2>itemsMappedToEntries</h2>
     <ol>
-      <li v-for="item in splitMappedFilteredItems">{{ item }}</li>
+      <li v-for="item in itemsMappedToEntries">{{ item }}</li>
+    </ol>
+    <h2>filteredEntries</h2>
+    <ol>
+      <li v-for="item in filteredEntries">{{ item }}</li>
     </ol>
     <h2>pretext</h2>
     <textarea style="border: 1px solid red; field-sizing: content">{{
