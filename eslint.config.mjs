4379730c495f1f9dc058a5ce05af09644f65cf56
CCC <chad.crume@gmail.com>
2025-04-20 17:47:57 -0400

    Added eslint-config-prettier, eslint-plugin-vue, globals, and vue-eslint-parser to correctly set up linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,43 @@
 import eslint from "@eslint/js";
 import { globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
-
-// export default defineConfig([globalIgnores([".config/"])]);
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
-  [globalIgnores(["vite.config.js", ".vite/", "public/dropbox-examples/", ".dependency-cruiser.cjs"])],
+  [
+    globalIgnores([
+      "vite.config.js",
+      ".vite/",
+      "public/dropbox-examples/",
+      ".dependency-cruiser.cjs",
+    ]),
+  ],
   eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
+  ...tseslint.configs.recommended,
+  // tseslint.configs.strict,
+  // tseslint.configs.stylistic,
+  ...pluginVue.configs["flat/recommended"],
+  {
+    plugins: {
+      "typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      // override/add rules settings here, such as:
+      // 'vue/no-unused-vars': 'error'
+    },
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        project: "./tsconfig.json",
+        extraFileExtensions: [".vue"],
+        sourceType: "module",
+        globals: {
+          ...globals.browser,
+        },
+      },
+    },
+  },
+  eslintConfigPrettier
 );
