fb29a5c026d53a9b2d90d5bb02b7ca27cbf24093
CCC <chad.crume@gmail.com>
2025-06-01 07:52:03 -0400

    bug: Type error on function ref

--- src/components/FlogEntry.vue
+++ src/components/FlogEntry.vue
@@ -19,7 +19,7 @@
     <pre
       v-else
       id="editEntry"
-      :ref="bindEntryEl"
+      ref="bindEntryEl"
       class="entry__body"
       :contenteditable="!isReadOnly"
       @blur="handleBlur"
@@ -107,7 +107,7 @@ const entryText = ref<string>(props.entry.entry);
 const isReadOnly = ref<boolean | null>(props.readOnly);
 const entryEl = ref<HTMLElement | null>(null);
 
-const bindEntryEl = (el: HTMLElement) => {
+const bindEntryEl = (el: HTMLElement | null) => {
   entryEl.value = el;
   // if (el) el.focus();
 };
